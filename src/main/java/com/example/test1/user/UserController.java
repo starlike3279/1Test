f7fb813dad8bbad452012741164cbfff361dee7a
taeho5825 <xogh332@naver.com>
2024-09-24 12:40:03 +0900

    ex2

--- src/main/java/com/example/test1/user/UserController.java
+++ src/main/java/com/example/test1/user/UserController.java
@@ -4,6 +4,7 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,10 +17,22 @@ public class UserController {
 
     private final UserService userService;
 
+    @GetMapping("/login")
+    public String login() {
+        return "login_form";
+    }
+
     @GetMapping("/signup")
     public String signup(UserCreateForm userCreateForm) {
         return "signup_form";
     }
+
+    @GetMapping("/signup")
+    public String signup(Model model) {
+        model.addAttribute("userCreateForm", new UserCreateForm());
+        return "signup_form";
+    }
+
 
     @PostMapping("/signup")
     public String signup(@Valid UserCreateForm userCreateForm,
--- src/main/java/com/example/test1/user/UserService.java
+++ src/main/java/com/example/test1/user/UserService.java
@@ -1,6 +1,7 @@
 package com.example.test1.user;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
